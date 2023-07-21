5556ba37db539cf7a65f04f8f4e2509647e5891d
matheus <matheus.henrique@elabbora.com.br>
2023-07-21 15:12:13 -0300

    cORREÇÕES

--- src/main/java/com/loiane/dto/AlunoDTO.java
+++ src/main/java/com/loiane/dto/AlunoDTO.java
@@ -2,12 +2,15 @@ package com.loiane.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import com.loiane.model.Course;
 import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.Length;
 
 public record AlunoDTO(
 
         @JsonProperty("idAluno") Long idAluno,
+
+
 
 
         @NotBlank @NotNull @Length(min = 5, max = 100) String nome,
@@ -20,5 +23,9 @@ public record AlunoDTO(
 
         //alunos
         @NotNull Integer idade,
-        @NotNull @Length(min = 16, max = 100)  String cpf) {
+        @NotNull @Length(min = 11, max = 100)  String cpf,
+
+        @JsonProperty("idCourse")
+        Course idCourse
+        ) {
 }
