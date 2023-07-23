23bf6a14dcfe84fb4b8d7a541307cd1259ade029
Matheus <iftx159@gmail.com>
2023-07-23 10:57:12 -0300

    Controller: Finaliza implementaçao de setor

--- src/main/java/com/loiane/dto/mapper/SetorMapper.java
+++ src/main/java/com/loiane/dto/mapper/SetorMapper.java
@@ -1,4 +1,32 @@
 package com.loiane.dto.mapper;
 
+import com.loiane.dto.SetorDTO;
+import com.loiane.model.Setor;
+
 public class SetorMapper {
+
+    public SetorDTO toDTO(Setor setor) {
+        if (setor == null) {
+            return null;
+        }
+        return new SetorDTO(setor.getId(), setor.getSetor());
+    }
+
+    public Setor toEntity(SetorDTO setorDTO) {
+
+        if (setorDTO == null) {
+            return null;
+        }
+
+        Setor setor = new Setor();
+        if (SetorDTO.class() != null) {
+            setor.setId(SetorDTO.id());
+        }
+        setor.setId(SetorDTO.id());
+        setor.setSetor(SetorDTO.setor());
+
+        return setor;
+    }
 }
+
+
